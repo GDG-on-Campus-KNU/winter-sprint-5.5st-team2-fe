0777ec9ee58a6e2ecea6d6ba57a9e2f94bd7c265
Jaemin Kim <jaemin@Jaeminui-MacBookAir.local>
2026-03-02 16:00:41 +0900

    refact: userName api와 연동

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -2,12 +2,31 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 import { clearTokens } from '../api/client';
 
+const normalizeUser = (userData = {}, prevUser = {}) => {
+  const mergedUser = { ...prevUser, ...userData };
+  const normalizedName =
+    mergedUser.userName ??
+    mergedUser.name ??
+    prevUser.userName ??
+    prevUser.name;
+
+  return {
+    ...mergedUser,
+    userName: normalizedName ?? '',
+    name: normalizedName ?? '',
+  };
+};
+
 const useAuthStore = create(
   persist(
     (set) => ({
       isLoggedIn: false,
       user: null,
-      setAuth: (userData) => set({ isLoggedIn: true, user: userData }),
+      setAuth: (userData) =>
+        set((state) => ({
+          isLoggedIn: true,
+          user: normalizeUser(userData ?? {}, state.user ?? {}),
+        })),
       logout: () => {
         clearTokens();
         set({ isLoggedIn: false, user: null });
